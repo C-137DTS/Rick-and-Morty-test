b6527cdcfc4b1439d46a7d68203611e7eac9f713
C-137DTS <guillenfortich@gmail.com>
2020-09-23 23:36:38 -0500

    Corrigiendo los bugs del juego

--- game.js
+++ game.js
@@ -1,58 +1,89 @@
-const VERIFICAR = document.querySelector('#verificar')
 var character_img = document.querySelector('#character_img')
 const API = 'https://rickandmortyapi.com/api/character/'
+const btn_verificar = document.querySelector('#verificar')
+const respuesta = document.querySelector('#respuesta')
 
+var secuencia = new Array(10).fill(0).map(n => Math.floor((Math.random() * 617) + 1))
+var nivel = 1
+var subnivel = 0
+
+function fetchData (url_api)  {
+   return new Promise((resolve, reject) => {
+       const xhttp = new XMLHttpRequest()
+       xhttp.open('GET', url_api, true)
+       xhttp.onreadystatechange = event => {
+           if(xhttp.readyState === 4) {
+               (xhttp.status === 200)
+                   ? resolve(JSON.parse(xhttp.responseText))
+                   : reject(new Error('Error: ', url_api))
+           }
+       }
+       xhttp.send()
+   })
+}
+
+async function obtenerImagen (personaje) {
+  var data = await fetchData(API + secuencia[personaje])
+  var img_url = data.image
+  return img_url
+}
+
+async function obtenerNombrePersonaje(personaje) {
+  var data = await fetchData(API + secuencia[personaje])
+  var character_name = data.name
+  return character_name
+}
+
+
+//
 class juego {
-    constructor() {
-        this.incializar()
-        this.generarSecuencia()
+     constructor() {
+       this.inicializar = this.inicializar.bind(this)
+       this.verificar = this.verificar.bind(this)
+       this.inicializar()
     }
 
-     fetchData (url_api)  {
-        return new Promise((resolve, reject) => {
-            const xhttp = new XMLHttpRequest()
-            xhttp.open('GET', url_api, true)
-            xhttp.onreadystatechange = event => {
-                if(xhttp.readyState === 4) {
-                    (xhttp.status === 200)
-                        ? resolve(JSON.parse(xhttp.responseText))
-                        : reject(new Error('Error: ', url_api))
-                }
-            }
-            xhttp.send()
-        })
-    }
-
-    incializar() {
-        this.nivel = 1
-        this.subnivel = 0
+    inicializar() {
         this.mostrarImagen()
-    }
-
-    generarSecuencia() {
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor((Math.random() * 617) + 1))
-        console.log(this.secuencia)
-        this.image_url = new URL(API + this.secuencia[0])
-        console.log(this.image_url)  //Esta variable será la encargada de almacenar las url de cada personaje
+        btn_verificar.addEventListener("click", this.verificar)
     }
 
     async mostrarImagen() {
-        try {
-          // El problema ocurre cuando le paso this.image_url como parametro a fetchData
-            var data = await this.fetchData(y.href)
-            var img_src = data.image
-            character_img.src = img_src
-        } catch (error) {
-            console.error(error)
+      try {
+        this.img_src = await obtenerImagen(subnivel)
+        character_img.src = this.img_src
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async verificar() {
+      try {
+        this.character_name = await obtenerNombrePersonaje(subnivel)
+        console.log(this)
+
+        if (this.character_name === respuesta.value || this.character_name.toLowerCase() === respuesta.value) {
+          console.log(`Felicidades haz completado el nivel ${nivel}`);
+          nivel ++
+          subnivel ++
+          respuesta.value = ''
+          this.inicializar()
+        } else {
+          console.log(`Lo siento Haz perdido. El nombre del personaje es ${this.character_name}`)
         }
+      } catch (error) {
+        console.error(error);
+      }
     }
 }
 
-var x = new Array(10).fill(0).map(n => Math.floor((Math.random() * 617) + 1))
-var y = new URL(API + x[0])
 
 function start () {
     window.juego = new juego()
+}
+
+function mensaje () {
+  alert('Estoy funcionando')
 }
 
 window.onload = start
